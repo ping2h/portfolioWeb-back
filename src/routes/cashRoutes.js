d3f3b0ccee55f1e7bce5ed13b216acfd8b2a67af
abc <754956348a@gmail.com>
2025-07-30 09:48:40 +0800

    WP-2 <cash deposit and withdraw>

--- src/routes/cashRoutes.js
+++ src/routes/cashRoutes.js
@@ -6,5 +6,7 @@ const router = express.Router();
 // GET /cash/income/:months 获取最近n个月现金流收入
 router.get('/income/:months', cashController.getIncomeByRecentMonths);
 router.get('/outcome/:months', cashController.getOutcomeByRecentMonths);
+router.put('/deposit/:amount', cashController.depositCash);
+router.put('/withdraw/:amount', cashController.withdrawCash);
 
 export default router;
